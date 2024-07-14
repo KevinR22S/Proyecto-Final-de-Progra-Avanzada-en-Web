38290f71d5b6559e38849a3f24cf190db4b6bfcc
KevinR22S <kramirez10960@ufide.ac.cr>
2024-07-13 20:32:09 -0600

    Solución de errores en Program

--- Proyecto_Final_Progra_Web/Proyecto_Final_Progra_Web/Program.cs
+++ Proyecto_Final_Progra_Web/Proyecto_Final_Progra_Web/Program.cs
@@ -1,30 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Proyecto_Final_Progra_Web.Models;
-using Microsoft.Extensions.Configuration;
-using Proyecto_Final_Progra_Web.Repositories;
-using Proyecto_Final_Progra_Web.Services; 
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("Conn") ?? throw new InvalidOperationException("Connection string 'Conn' not found.");
 
 builder.Services.AddDbContext<ProyectoFinalWebContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
-
-builder.Services.AddScoped<IJuegoService, JuegoService>();
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Login/IniciarSesion";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+    });
 
 var app = builder.Build();
 
-// Configurar el pipeline HTTP.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
@@ -43,7 +40,6 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-// Configuración de las rutas de controladores
 app.MapControllerRoute(
     name: "usuarios",
     pattern: "Usuarios/{action=Index}/{id?}",
--- Proyecto_Final_Progra_Web/Proyecto_Final_Progra_Web/obj/Debug/net8.0/Proyecto_Final_Progra_Web.AssemblyInfo.cs
+++ Proyecto_Final_Progra_Web/Proyecto_Final_Progra_Web/obj/Debug/net8.0/Proyecto_Final_Progra_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Final_Progra_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9109b5ddd4e32684f4cf7240b17b2c689bae8495")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91812c2e973a1c8a7a716d9bbfe49cf5bc39aaa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Final_Progra_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Final_Progra_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
