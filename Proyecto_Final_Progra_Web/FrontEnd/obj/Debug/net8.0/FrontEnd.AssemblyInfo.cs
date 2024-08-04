3e35bdc80fd452c3eb3eac8d9cbda17818823848
ying-80 <ying_07@outlook.com>
2024-08-04 14:15:42 -0600

    login1

--- Proyecto_Final_Progra_Web/FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
+++ Proyecto_Final_Progra_Web/FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe6802e2e8f4c9320fe0814eae8f425d649f5386")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74d50f02b115a86c48d82010d31cf457ee95bb65")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
